bdc39c0b426de7aab22146403ce23051559f2865
Holger H <holgheiss@gmail.com>
2022-09-02 10:32:54 +0200

    Revert "fix columns initial condition"
    This reverts commit e1a78f762980db28ba9eb3f636eabe08c663c66d.

--- src/components/pageLayout.js
+++ src/components/pageLayout.js
@@ -43,7 +43,7 @@ export const PageLayout = ({ page, children, left, right }) => {
   return (
     <>
       {pageTitle && <SEO title={pageTitle} />}
-      { right && right.length > 0 ?
+      { right.length > 0 ?
         <Container style={{ height: `${100 * vh}px` }}>
           <Left>
             {left}
